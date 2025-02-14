35eeebc8830340d755c5b6bd372ecb921614de90
sagarPakhrin <sagarlama826@gmail.com>
2025-02-14 11:40:57 +0545

    fix secrity issues

--- json.go
+++ json.go
@@ -19,13 +19,18 @@ func respondWithError(w http.ResponseWriter, code int, msg string) {
 }
 
 func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
-	w.Header().Set("Content-Type", "application/json")
 	dat, err := json.Marshal(payload)
 	if err != nil {
-		log.Printf("Error marshalling JSON: %s", err)
-		w.WriteHeader(500)
+		log.Printf("Error marshaling JSON: %v", err)
+		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
+
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(code)
-	w.Write(dat)
+	if _, err := w.Write(dat); err != nil {
+		log.Printf("Error writing response: %v", err)
+		// Note: We can't write a new header at this point as it's already been sent
+		// Best we can do is log the error
+	}
 }
